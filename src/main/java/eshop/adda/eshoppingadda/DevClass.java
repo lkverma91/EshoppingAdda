0ee1848e0e54d722d4daae1743ee6a399c2e3c80
Krishan <lkverma91@gmail.com>
2025-07-15 13:41:18 +0530

    dev branch initiall commit, added DevClass only to test

---
+++ src/main/java/eshop/adda/eshoppingadda/DevClass.java
@@ -0,0 +1,10 @@
+package eshop.adda.eshoppingadda;
+
+public class DevClass {
+    //create default constructor
+    public DevClass() {
+        // write a message to indicate that the class has been initialized
+        System.out.println("DevClass initialized"); 
+    }
+    
+}
